f7ef44b3a4893c24b51e05b1ac74c6c0e6aa7c70
miguelphdz <miguelhernandez353@outlook.com>
2025-03-18 23:29:48 -0600

    TESTING

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
-Route::apiResource(
-    'users',
-    UserController::class
-);
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('/users', [UserController::class, 'store']);
+
